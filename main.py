366058aa4cfe98515e8593d8747d3dc190685fc6
CtripaClip <82753111+CtripaClip@users.noreply.github.com>
2023-07-20 20:49:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# Язык Python
-
 from aiogram import Bot, types, Dispatcher, executor
 
 TOCEN = 'Ваш токен с BotFather'
@@ -10,10 +8,9 @@ dp = Dispatcher(bot)
 # создаём эхо-бота
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.message):
-    # Выводим на экран "Привет это эхо бот"
-    await bot.send_message(message.from_user.id, "Привет это эхо бот")
-    # Отправляем ему его сообщение
-    await message.answer(message.text)
+    
+    await bot.send_message(message.from_user.id, "Привет это эхо бот")# Выводим на экран "Привет это эхо бот"
+    await bot.answer(message.text) # Отправляем ему его сообщение
 
 # задаём что наш бот будет работать только когда мы его включаем
 executor.start_polling(dp, skip_updates=False)
